b77c802f6446e0c53f41a75a3f125b1763ed9958
Fariba <faribamohamadi.mr@gmail.com>
2023-08-14 18:20:29 +0330

    creat file for week names

---
+++ main.py
@@ -0,0 +1,11 @@
+
+"""
+enter number and see name of day
+"""
+
+
+days = [' ', 'saturday', 'sunday', 'monday', 'teusday', 'wendnesday', 'thursday', 'friday']
+num = int(input('Enter number between 1-7 :'))
+day = days[num]
+print(f'{num}th day of the week is {day} .')
+
